d9e92016fd713d1d9c9e64338a9b48193f9028a7
Canh Huynh <huynhthecanhpvh@gmail.com>
2025-12-04 10:40:44 +0700

    create layout for project, login, register, models

---
+++ manage_gym_app/gymapp/dao.py
@@ -0,0 +1,30 @@
+import hashlib
+
+import cloudinary
+
+from gymapp import db, app
+from gymapp.models import User
+
+
+def get_user_by_id(id):
+    return User.query.get(id)
+
+def auth_user(username, password):
+    password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+    return User.query.filter(User.username==username.strip(),
+                             User.password==password).first()
+
+
+def add_user(name, username, password, avatar):
+    u = User(name=name,
+             username=username.strip(),
+             password=str(hashlib.md5(password.strip().encode('utf-8')).hexdigest()))
+
+    if avatar:
+        res = cloudinary.uploader.upload(avatar)
+        u.avatar = res.get('secure_url')
+
+    db.session.add(u)
+    db.session.commit()
+
+
---
+++ manage_gym_app/gymapp/index.py
@@ -0,0 +1,68 @@
+from flask import render_template, request, redirect
+from flask_login import logout_user, login_user
+
+from gymapp import app, dao, login
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',)
+
+
+@app.route('/login')
+def login_view():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register_view():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['post'])
+def register_process():
+    password = request.form.get('password')
+    confirm = request.form.get('confirm')
+
+    if password != confirm:
+        err_msg = 'Mật khẩu KHÔNG khớp'
+        return render_template('register.html', err_msg=err_msg)
+
+    avatar = request.files.get('avatar')
+    try:
+        dao.add_user(avatar=avatar,
+                    name=request.form.get('name'),
+                    username=request.form.get('username'),
+                    password=request.form.get('password'))
+    except Exception as ex:
+        return render_template('register.html', err_msg="Hệ thống đang có lỗi!")
+
+    return redirect('/login')
+
+
+@app.route('/login', methods=['post'])
+def login_process():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    u = dao.auth_user(username=username, password=password)
+    if u:
+        login_user(user=u)
+
+    next = request.args.get('next')
+    return redirect(next if next else '/')
+
+
+@app.route('/logout')
+def logout_process():
+    logout_user()
+    return redirect('/login')
+
+@login.user_loader
+def load_user(pk):
+    return dao.get_user_by_id(pk)
+
+
+if __name__ == '__main__':
+    from gymapp import admin
+    app.run(debug=True)
